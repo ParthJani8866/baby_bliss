72c28206de6f13cec679841bc469445336df3ef4
Parth Jani <162541648+ParthJani8866@users.noreply.github.com>
2025-08-29 04:14:23 -0500

    site mapp added

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://truevivah.com',
+  generateRobotsTxt: true, // generates robots.txt automatically
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
