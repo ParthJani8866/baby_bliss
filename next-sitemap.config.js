f502e2122060d10dbae48a6f0a7e0134e607cbbb
Parth Jani <162541648+ParthJani8866@users.noreply.github.com>
2025-08-29 04:22:28 -0500

    New sitemap file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,16 @@
-/** @type {import('next-sitemap').IConfig} */
+const slugify = require('slugify');
+const { categories } = require('./data/categories');
+
 module.exports = {
   siteUrl: 'https://truevivah.com',
-  generateRobotsTxt: true, // generates robots.txt automatically
-  sitemapSize: 5000,
+  generateRobotsTxt: true,
   changefreq: 'daily',
   priority: 0.7,
+  additionalPaths: async (config) => {
+    return categories.map((category) => ({
+      loc: `/categories/${slugify(category.name, { lower: true })}`,
+      changefreq: 'daily',
+      priority: 0.8,
+    }));
+  },
 };
