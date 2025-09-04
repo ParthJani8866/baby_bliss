41bebeb91a0a76abae48d1be24c316111d4de988
Parth Jani <162541648+ParthJani8866@users.noreply.github.com>
2025-09-04 02:40:00 -0500

    Check for update

--- pages/_app.js
+++ pages/_app.js
@@ -14,11 +14,24 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="google-site-verification" content="cABz7WiOmmxhYSWi92HCqRHetXYjBdcGqlVm9VerfRc" />
       </Head>
 
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-693333660"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads-tag" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-693333660');
+        `}
+      </Script>
       {/* Google Analytics */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-VQ83YSY2P5"
         strategy="afterInteractive"
       />
+
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
