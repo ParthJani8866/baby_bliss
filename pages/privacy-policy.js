6805a5d70932676d7275cf76e00da1708b2aaa39
Parth Jani <162541648+ParthJani8866@users.noreply.github.com>
2025-09-25 04:35:08 -0500

    All Changed

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Head from "next/head";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export default function PrivacyPolicy() {
