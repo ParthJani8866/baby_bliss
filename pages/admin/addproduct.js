cd274681616c1c035e66a7afeaf172de16193e7c
Parth Jani <162541648+ParthJani8866@users.noreply.github.com>
2025-09-25 04:38:40 -0500

    Test

--- pages/admin/addproduct.js
+++ pages/admin/addproduct.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import axios from "axios";
-import Header from "../../pages/components/header";
+import Header from "../../pages/components/Header";
 
 export default function AddProductPage() {
   const [form, setForm] = useState({
