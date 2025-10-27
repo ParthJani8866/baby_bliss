2cb10d13dcc9437f02244214fe130a4fdb06afe2
Parth Jani <162541648+ParthJani8866@users.noreply.github.com>
2025-10-27 07:23:39 -0500

    Update

--- pages/components/AdBanner.js
+++ pages/components/AdBanner.js
@@ -3,23 +3,24 @@ import { useEffect } from "react";
 
 export default function AdBanner() {
   useEffect(() => {
-    // Dynamically load the EffectiveGate script
+    // Create Adsterra script dynamically
     const script = document.createElement("script");
     script.src = "//pl27936159.effectivegatecpm.com/6ecac58554a85ebd45aa5bb9ab11a5ae/invoke.js";
     script.async = true;
     script.setAttribute("data-cfasync", "false");
 
-    // Append script to body
+    // Append to document body
     document.body.appendChild(script);
 
-    // Cleanup to remove script if component unmounts
+    // Cleanup script on unmount
     return () => {
       document.body.removeChild(script);
     };
   }, []);
 
   return (
-    <div className="w-full flex justify-center">
+    <div className="w-full flex justify-center my-4">
+      {/* This is where Adsterra will inject the ad */}
       <div id="container-6ecac58554a85ebd45aa5bb9ab11a5ae"></div>
     </div>
   );
